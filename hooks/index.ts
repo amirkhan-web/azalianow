213423612d8d5bdc3291f732e44616d8f2dd0e84
amirkhan_web <amirhanvalman@gmail.com>
2023-02-01 21:06:06 +0300

    refactor-hooks

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,4 +1,4 @@
-import { AppDispatch } from './../redux/index';
+import { AppDispatch } from './../store/index';
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux"
 
 export const useAppDispatch: () => AppDispatch = useDispatch 
